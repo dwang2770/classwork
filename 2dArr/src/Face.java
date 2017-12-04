c83628193c67b8c3c32def5a028c1a2c5f85da5b
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-12-04 09:26:19 -0500

    sss

---
+++ 2dArr/src/Face.java
@@ -0,0 +1,19 @@
+
+public class Face {
+
+	public static void main(String[] args) {
+		int[][] xd = new int[6][7];
+		xd [1][0] = 1;
+		xd [2][1] = 1;
+		xd [3][0] = 1;
+		xd [1][2] = 1;
+		xd [3][2] = 1;
+		xd [1][4] = 1;
+		xd [2][4] = 1;
+		xd [3][4] = 1;
+		xd [1][1] = 1;
+		xd [1][1] = 1;
+		xd [1][1] = 1;
+	}
+
+}
