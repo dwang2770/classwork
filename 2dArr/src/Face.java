175f48414333a7dc4d905a1190bbaa555d706648
Dave <Dave@DESKTOP-OF579IP.fios-router.home>
2017-12-04 20:38:03 -0500

    sd

--- 2dArr/src/Face.java
+++ 2dArr/src/Face.java
@@ -2,7 +2,7 @@
 public class Face {
 
 	public static void main(String[] args) {
-		int[][] xd = new int[6][7];
+		int[][] xd = new int[5][7];
 		xd [1][0] = 1;
 		xd [2][1] = 1;
 		xd [3][0] = 1;
@@ -11,9 +11,19 @@ public class Face {
 		xd [1][4] = 1;
 		xd [2][4] = 1;
 		xd [3][4] = 1;
-		xd [1][1] = 1;
-		xd [1][1] = 1;
-		xd [1][1] = 1;
+		xd [1][5] = 1;
+		xd [3][5] = 1;
+		xd [2][6] = 3;
+	
+		for (int i = 0; i <xd.length; i++)
+			
+		{
+			for (int j=0;j < xd[i].length; j++)
+			{
+				System.out.print( xd [i][j]);
+			}
+			System.out.println();
+		}
 	}
 
 }
